ae98893250a43ab2616fc2dce40b98538629bcc2
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2025-08-27 18:58:46 -0700

    Title: Distinguishing Villages, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 250827/Distinguishing Villages/seperate-village.py
+++ 250827/Distinguishing Villages/seperate-village.py
@@ -1,49 +1,40 @@
+import heapq as hq
+
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
+deltas = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
-# Find connected components
-# using DFS starting at every valid position
-# never unmark from visited array
-# - can modify visited array directly on grid (maybe)
+cc_sizes = []
 
-visited = [
-    [False for _ in range(n)]
-    for _ in range(n)
-]
 
-deltas = [
-    (1, 0),  # down
-    (0, 1),  # right
-    (-1, 0), # up
-    (0, -1), # left
-]
-
-def can_go(r, c):
-    if r < 0 or c < 0 or r >= n or c >= n:
+def can_go(r: int, c: int):
+    if r < 0 or r >= n or c < 0 or c >= n:
         return False
-    if visited[r][c] or grid[r][c] == 0:
+    if grid[r][c] != 1:
         return False
     return True
 
-connected_component_sizes = []
-def dfs(r, c):
+# Dfs identifies the size of a connected component, marks it as seen
+def dfs(r: int, c: int) -> None:
+    grid[r][c] = 2 # Mark self as visited
     size = 1
+
     for dr, dc in deltas:
         nr, nc = r + dr, c + dc
-
         if can_go(nr, nc):
-            visited[nr][nc] = True
             size += dfs(nr, nc)
 
     return size
 
 for r in range(n):
     for c in range(n):
-        if not visited[r][c] and grid[r][c] == 1:
-            visited[r][c] = True
-            connected_component_sizes.append(dfs(r, c))
+        if grid[r][c] == 2: # Position already seen
+            continue
+        elif grid[r][c] == 1: # Connected component here: run dfs
+            hq.heappush(cc_sizes, dfs(r, c))
+        else:
+            grid[r][c] = 2 # Nothing here, mark it as seen
 
-            
-print(len(connected_component_sizes))
-for size in sorted(connected_component_sizes):
-    print(size)
+print(len(cc_sizes))
+while cc_sizes:
+    print(hq.heappop(cc_sizes))
