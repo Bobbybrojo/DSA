0c3e9803339408896887f60f72f8decb37edbf83
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2025-08-28 12:41:05 -0700

    Title: Determine the Possibility of Escaping in Four Directions, Time: 575ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 250828/Determine the Possibility of Escaping in Four Directions/determine-escapableness-with-4-ways.py
+++ 250828/Determine the Possibility of Escaping in Four Directions/determine-escapableness-with-4-ways.py
@@ -2,24 +2,23 @@ from collections import deque
 
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
 deltas = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 def can_go(r: int, c: int) -> bool:
     if r < 0 or c < 0 or r >= n or c >= m:
         return False
-    if visited[r][c] or grid[r][c] == 0:
+    if grid[r][c] == 0 or grid[r][c] == -1:
         return False
     return True
 
 q = deque()
 def bfs(r, c):
     q.append((r, c))
-    visited[r][c] = True
+    grid[r][c] = -1
 
     while q:
         r, c = q.popleft()
-        visited[r][c] = True
+        grid[r][c] = -1
 
         for dr, dc in deltas:
             nr, nc = r + dr, c + dc
@@ -28,4 +27,4 @@ def bfs(r, c):
                 q.append((nr, nc))
 
 bfs(0, 0)
-print(1 if visited[n - 1][m - 1] else 0)
+print(1 if grid[n - 1][m - 1] == -1 else 0)
