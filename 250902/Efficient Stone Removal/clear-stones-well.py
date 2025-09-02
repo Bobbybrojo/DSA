e781b67d5fd1e9fdc4a8abe0ae858f15fe645f40
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2025-09-02 14:22:43 -0700

    Title: Efficient Stone Removal, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250902/Efficient Stone Removal/clear-stones-well.py
@@ -0,0 +1,62 @@
+from collections import deque
+
+n, k, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+r = []
+c = []
+for _ in range(k):
+    ri, ci = map(int, input().split())
+    r.append(ri - 1)
+    c.append(ci - 1)
+starting_points = zip(r, c)
+
+# Returns whether the point is in the grid (0-indexed)
+def in_range(r, c):
+    return r >= 0 and c >= 0 and r < n and c < n
+
+# Returns the number of cells that can be visited from the given starting points
+def bfs(starting_points):
+    
+    visited = set()
+    q = deque(starting_points)
+
+    while q:
+        r, c = q.popleft()
+
+        for dr, dc in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            nr, nc = r + dr, c + dc
+
+            if in_range(nr, nc) and (nr, nc) not in visited and grid[nr][nc] == 0:
+                visited.add((nr, nc))
+                q.append((nr, nc))
+
+    return len(visited)
+
+# Get the position of all stones on the grid
+stones = []
+for r in range(n):
+    for c in range(n):
+        if grid[r][c] == 1:
+            stones.append((r, c))
+
+# For all combinations of m stones removed, run bfs
+def backtracking(count, last_idx):
+    global max_reachable
+    
+    if count == m:
+        reachable = bfs(starting_points)
+        max_reachable = max(max_reachable, reachable)
+        return
+
+    for i in range(last_idx + 1, len(stones)):
+        stone = stones[i]
+        
+        grid[stone[0]][stone[1]] = 0
+        backtracking(count + 1, i)
+        grid[stone[0]][stone[1]] = 1
+
+max_reachable = 0
+backtracking(0, -1)
+
+print(max_reachable)
