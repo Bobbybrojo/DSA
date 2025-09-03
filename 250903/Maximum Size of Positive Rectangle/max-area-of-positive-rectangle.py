d8861a32afab7bbeb6e297623f195157dbbe3106
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2025-09-03 15:49:12 -0700

    Title: Maximum Size of Positive Rectangle, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250903/Maximum Size of Positive Rectangle/max-area-of-positive-rectangle.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+rects = []
+for r1 in range(n):
+    for r2 in range(r1, n):
+        for c1 in range(m):
+            for c2 in range(c1, m):
+                rects.append((r1, c1, r2, c2))
+
+def positive_rect(rect):
+    r1, c1, r2, c2 = rect
+
+    for r in range(r1, r2 + 1):
+        for c in range(c1, c2 + 1):
+            if grid[r][c] < 0:
+                return False
+
+    return True
+
+max_area = -1
+for rect in rects:
+    if positive_rect(rect):
+        r1, c1, r2, c2 = rect
+        max_area = max((r2 - r1 + 1) * (c2 - c1 + 1), max_area)
+
+print(max_area)
