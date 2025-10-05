b8a5c875869c3d9367a8c384080867c99e09ce83
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2025-10-05 14:05:07 -0700

    Title: Coin Addition, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251005/Coin Addition/add-coins.py
+++ 251005/Coin Addition/add-coins.py
@@ -5,16 +5,13 @@ coins = [int(input()) for _ in range(n)]
 
 coins_used = 0
 target = k
-value = 0
 coin_idx = -1
 
-while value != k:
-
+while target != 0:
     coin = coins[coin_idx]
     num_coins = int(target // coin)
     if num_coins >= 1:
         target = target - (num_coins * coin)
-        value += (num_coins * coin)
         coins_used += num_coins
     else:
         coin_idx -= 1
