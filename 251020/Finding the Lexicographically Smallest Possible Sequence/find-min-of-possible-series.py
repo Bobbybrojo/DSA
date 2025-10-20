c11f864677597219fa4e650667713d2e4c45f024
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2025-10-20 13:15:40 -0700

    Title: Finding the Lexicographically Smallest Possible Sequence, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 251020/Finding the Lexicographically Smallest Possible Sequence/find-min-of-possible-series.py
+++ 251020/Finding the Lexicographically Smallest Possible Sequence/find-min-of-possible-series.py
@@ -1,15 +1,16 @@
 n = int(input())
 
+# Backtracking algorithm
 def backtrack(curr):
-    global min_valid_seq
 
+    # Return the first sequence we find (the smallest one)
     if len(curr) == n:
-        min_valid_seq = min(min_valid_seq, curr)
-        return
+        return curr
 
     for i in range(4, 6 + 1):
         curr += str(i)
 
+        # Check if sequence is valid
         is_invalid = False
         for l in range(2, len(curr) + 1, 2):
             hl = l//2
@@ -17,12 +18,17 @@ def backtrack(curr):
                 is_invalid = True
                 break
         
+        # If valid backtrack
         if not is_invalid:
-            backtrack(curr)
+            result = backtrack(curr)
+            if result:
+                return result
 
         curr = curr[:-1]
 
+    # Return None if no valid sequences
+    return None
 
-min_valid_seq = "6" * n
-backtrack("")
-print(min_valid_seq)
+
+result = backtrack("")
+print(result if result else "6" * n)
