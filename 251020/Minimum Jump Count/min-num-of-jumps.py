df14f9214566ae03d16baa34f83aff6632aba3e2
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2025-10-20 13:38:37 -0700

    Title: Minimum Jump Count, Time: 83ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 251020/Minimum Jump Count/min-num-of-jumps.py
@@ -0,0 +1,18 @@
+n = int(input())
+num = list(map(int, input().split()))
+
+def backtrack(pos, cnt):
+    global min_jump_count
+
+    if pos == n - 1:
+        min_jump_count = min(min_jump_count, cnt)
+        return
+
+    max_jump_dist = num[pos]
+    for i in range(1, max_jump_dist + 1):
+        backtrack(pos + i, cnt + 1)
+
+
+min_jump_count = n
+backtrack(0, 0)
+print(min_jump_count if min_jump_count != n else -1)
