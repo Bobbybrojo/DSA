4a734aac1664195dc79e8097ec7a15ff222b0685
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2025-10-21 13:11:45 -0700

    Title: Selecting M out of N, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251021/Selecting M out of N/n-choose-m.py
+++ 251021/Selecting M out of N/n-choose-m.py
@@ -8,12 +8,8 @@ def combinations(curr, last_idx):
         return
 
     for i in range(last_idx + 1, N + 1):
-        if i not in visited:
-            visited.add(i)
-            curr.append(i)
-            combinations(curr, i)
-            visited.remove(i)
-            curr.pop()
+        curr.append(i)
+        combinations(curr, i)
+        curr.pop()
 
-visited = set()
 combinations([], 0)
