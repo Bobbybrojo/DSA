54037168d52daa885169558043262eda0d820ced
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2025-10-22 12:53:19 -0700

    Title: Traveling Salesman Problem, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251022/Traveling Salesman Problem/traveling-salesman-problem.py
@@ -0,0 +1,40 @@
+import sys
+
+n = int(input())
+A = [list(map(int, input().split())) for _ in range(n)]
+
+# backtracking: O(nP(n,n))
+
+def backtrack(curr):
+    if len(curr) == n:
+        curr.append(0)
+        permutations.append(curr[:])
+        curr.pop()
+        return
+
+    for i in range(0, n):
+        if not visited[i]:
+            visited[i] = True
+            curr.append(i)
+            backtrack(curr)
+            visited[i] = False
+            curr.pop()
+    return
+
+# Fill the permutations list with all orderings of visiting nodes
+visited = [False] * n
+visited[0] = True
+permutations = []
+backtrack([0])
+
+# Calculate min cost from all permutations
+min_sum = sys.maxsize
+for perm in permutations:
+    perm_sum = 0
+    
+    for i in range(1, len(perm)):
+        perm_sum += A[perm[i - 1]][perm[i]]
+
+    if perm_sum < min_sum:
+        min_sum = perm_sum
+print(min_sum)
