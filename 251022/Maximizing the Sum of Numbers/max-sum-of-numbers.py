4c9b0405dd4c860c41badfba2622cd0a89f39f32
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2025-10-21 22:08:36 -0700

    Title: Maximizing the Sum of Numbers, Time: 1090ms, Memory: 23MB, Status: Time Limit Exceed - Codetree

--- 251022/Maximizing the Sum of Numbers/max-sum-of-numbers.py
+++ 251022/Maximizing the Sum of Numbers/max-sum-of-numbers.py
@@ -2,20 +2,61 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 
+# THIRD SOLUTION: Optimized backtracking (Only backtrack on possible solutions)
+# Runtime: O(P(n^2, n) * n)
+visited_col = [False] * n
+visited_row = [False] * n
+
+def backtracking(curr):
+    global max_sum
+
+    if len(curr) == n:
+        max_sum = max(max_sum, sum(curr)) # O(n) for summing list for each result
+        return
+
+    for c in range(n):
+        if visited_col[c]:
+            continue
+
+        for r in range(n):
+            if visited_row[r]:
+                continue
+            
+            visited_row[r] = True
+            visited_col[c] = True
+            curr.append(grid[r][c])          
+
+            backtracking(curr)
+
+            visited_row[r] = False
+            visited_col[c] = False
+            curr.pop()
+    return
+
+max_sum = -1
+backtracking([])
+print(max_sum)
+
+
+# RESULT: Does not cover the entire solution space
 # SECOND SOLUTION: Result: Faster Overall
 # Ad hoc: simulate a diagonal on the grid and move it once to the right for each permutation
 # Use modulo to wrap chosen column
 # Presumed runtime O(n^2)
 
-max_sum = -1
-c = 0
-for start in range(n):
-    curr = []
-    for r in range(start, n + start):
-        curr.append(grid[r % n][c % n])
-        c += 1
-    max_sum = max(max_sum, sum(curr))
-print(max_sum)
+# max_sum = -1
+# visited = [False] * n
+# for r in range(n):
+#     curr = []
+#     for c in range(n):
+#         if visited[c]:
+#             continue
+
+#         curr.append(grid[r][c])
+
+#     max_sum = max(max_sum, sum(curr))
+#     print(curr, sum(curr))
+# print(max_sum)
 
 # FIRST SOLUTION
 # Brute force backtracking: O(P(n, n) * n) : PASSES
