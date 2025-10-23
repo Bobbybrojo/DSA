bb9008986901ac6f54a8bad24e319b3131c91c75
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2025-10-22 19:23:38 -0700

    Title: Maximizing the Minimum of Selected Numbers, Time: 199ms, Memory: 24MB, Status: Passed - Codetree

--- 251022/Maximizing the Minimum of Selected Numbers/maximin-of-numbers.py
+++ 251022/Maximizing the Minimum of Selected Numbers/maximin-of-numbers.py
@@ -2,7 +2,7 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Backtrack and track current min throughout function: O(P(n, n))
-def backtracking(curr_min, cnt):
+def backtrack(curr_min, cnt):
     global result
 
     if cnt == n:
@@ -14,11 +14,13 @@ def backtracking(curr_min, cnt):
             visited[r] = True
             if curr_min == None:
                 curr_min = grid[r][cnt]
-            backtracking(min(curr_min, grid[r][cnt]), cnt + 1)
+            backtrack(min(curr_min, grid[r][cnt]), cnt + 1)
             visited[r] = False
+            if cnt == 0:
+                curr_min = None
     return
 
 visited = [False] * n
 result = -1
-backtracking(None, 0)
+backtrack(None, 0)
 print(result)
