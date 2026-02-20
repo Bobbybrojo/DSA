1d82b04733b40eb369bcf8226d64552d508a1ed3
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2026-02-19 17:04:23 -0800

    Title: Determine if a String is a Palindrome, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260219/Determine if a String is a Palindrome/determine-whether-palindrome-is-present.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPalindrome(string s) {
+    int n = s.size();
+    for (int i{}; i < n / 2; ++i) {
+        if (s[i] != s[n - 1 - i]) return false;
+    }
+    return true;
+}
+
+int main() {
+    string A;
+    cin >> A;
+
+    isPalindrome(A) ? cout << "Yes" : cout << "No";
+
+    return 0;
+}
