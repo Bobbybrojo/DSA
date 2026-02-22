e90b628097b04f8165fd1c208912740da7c8c0c8
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2026-02-21 17:40:38 -0800

    Title: Maximum Overlapping Interval, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260221/Maximum Overlapping Interval/maximum-overlapped-segments.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    int n;
+    int x1[100], x2[100];
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x1[i] >> x2[i];
+    }
+
+    int slots[200]{0};
+    for (int i{}; i < n; ++i) {
+        for (int j{x1[i]}; j <= x2[i]; ++j) {
+            slots[j] += 1;
+        }
+    }
+
+    int max = *std::max_element(slots, slots + 200);
+    cout << max;
+
+
+    return 0;
+}
