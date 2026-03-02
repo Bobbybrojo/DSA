0b98d79928f0e0e163b27f0135cc65c513e65a76
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2026-03-02 15:29:01 -0800

    Title: Consecutive Numbers 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260302/Consecutive Numbers 2/continuous-number2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    int N;
+    int arr[1000];
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    int count{1};
+    int max_count{0};
+    int last;
+    for (int i{0}; i < N; ++i) {
+        if (i == 0) {
+            count++;
+        }
+        else if (last == arr[i]) {
+            count++;
+        } else {
+            count = 1;
+        }
+        max_count = max(max_count, count);
+        last = arr[i];
+    }
+
+    cout << max_count;
+
+    return 0;
+}
