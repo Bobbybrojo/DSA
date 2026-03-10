23a7a85cee84460965fefc79631798ba9c393947
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2026-03-09 21:14:11 -0700

    Title: Implement Merge Sort, Time: 328ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 260310/Implement Merge Sort/implement-merge-sort.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+void merge(int arr[], int mergedArr[], int low, int mid, int high) {
+    int i = low, j = mid + 1;
+
+    int k = low;
+    while (i <= mid && j <= high) {
+        if (arr[i] <= arr[j]) {
+            mergedArr[k] = arr[i];
+            i++;
+            k++;
+        } else {
+            mergedArr[k] = arr[j];
+            j++;
+            k++;
+        }
+    }
+
+    while (i <= mid) {
+        mergedArr[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    while (j <= high) {
+        mergedArr[k] = arr[j];
+        j++;
+        k++;
+    }
+
+    std::copy(mergedArr, mergedArr + high + 1, arr);
+}
+
+void mergeSort(int arr[], int mergedArr[], int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+        mergeSort(arr, mergedArr, low, mid);
+        mergeSort(arr, mergedArr, mid + 1, high);
+        merge(arr, mergedArr, low, mid, high);
+    }
+}
+
+int n;
+int arr[100000];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Perform merge sort
+    int mergedArr[n];
+    mergeSort(arr, mergedArr, 0, n - 1);
+
+    // Print result
+    for (int i{}; i < n; ++i) {
+        cout << mergedArr[i] << ' ';
+    }
+
+    return 0;
+}
+
+
