539e80a3d14f856c72f25b4366b6c363f21d68a2
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2026-03-31 16:37:38 -0700

    Title: 정수 사각형 최댓값의 최소, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260331/정수 사각형 최댓값의 최소/minimax-path-in-square.cpp
+++ 260331/정수 사각형 최댓값의 최소/minimax-path-in-square.cpp
@@ -23,13 +23,13 @@ int main() {
     // Initialize dp table
     dp[0][0] = grid[0][0];
     for (int i{1}; i < n; ++i) {
-        dp[0][i] = max(grid[0][i], grid[0][i - 1]);
-        dp[i][0] = max(grid[i][0], grid[i - 1][0]);
+        dp[0][i] = max(grid[0][i], dp[0][i - 1]);
+        dp[i][0] = max(grid[i][0], dp[i - 1][0]);
     }
 
     // Fill dp table by tabulation
-    for (int r{}; r < n; ++r) {
-        for (int c{}; c < n; ++c) {
+    for (int r{1}; r < n; ++r) {
+        for (int c{1}; c < n; ++c) {
             dp[r][c] = max(grid[r][c], min(dp[r - 1][c], dp[r][c-1]));
         }
     }
