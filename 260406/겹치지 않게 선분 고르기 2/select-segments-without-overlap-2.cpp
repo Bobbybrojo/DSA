ca944369fdc8d58a18e67e667bc98e935a25658c
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2026-04-05 21:53:02 -0700

    Title: 겹치지 않게 선분 고르기 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260406/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
+++ 260406/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
@@ -8,9 +8,7 @@ int x1[1000];
 int x2[1000];
 
 bool isOverlapping(int x1, int x2, int y1, int y2) {
-    if (y1 <= x2 && y1 >= x1) return true;
-    if (y2 <= x2 && y2 >= x1) return true;
-    return false;
+    return !(x2 < y1 || y2 < x1);
 }
 
 int main() {
@@ -32,7 +30,7 @@ int main() {
 
     // Perform Recurrence
     for (int i{1}; i < n; ++i) {
-        int maxNum{-1};
+        int maxNum{1};
         for (int j{}; j < i; ++j) {
             if (dp[j] != -1 && !isOverlapping(x1[i], x2[i], x1[j], x2[j])) {
                 maxNum = max(maxNum, dp[j] + 1);
