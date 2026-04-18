16fe6a46edc43f0873155ef484731d517b1c653b
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2026-04-18 14:30:58 -0700

    Title: 동전 거슬러주기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260418/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int n;
+int m;
+int coins[100];
+
+int main() {
+    
+    cin >> n >> m;
+    for (int i{}; i < n; ++i) {
+        cin >> coins[i];
+    }
+
+    // Sort coins
+    sort(coins, coins + n);
+
+    // DP[i] represents the minimum number of coins needed to make value i
+    // DP[i] = DP[j] + 1 for all j < i if coins contains C where j + C = i
+
+    int dp[10001];
+    dp[0] = 0;
+    for (int i{1}; i <= m; ++i) {
+        dp[i] = -1;
+    }
+
+    for (int i{1}; i <= m; ++i) {
+        dp[i] = INT_MAX;
+        for (int j{}; j < i; ++j) {
+            for (int c{}; c < n; ++c) {
+                if (j + coins[c] == i && dp[j] != -1) {
+                    dp[i] = min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+    }
+
+    int ans = dp[m] != INT_MAX ? dp[m] : -1;
+    cout << ans;
+
+    
+
+    return 0;
+}
