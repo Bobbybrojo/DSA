8f68a565ac3224843a17f6348e93263b875aa724
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2026-04-23 10:47:55 -0700

    Title: 1, 2, 5 더하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260423/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int arr[3] = {1, 2, 5};
+
+int main() {
+    cin >> n;
+
+    // DP[i] represents the number of ways to express integer i as a sum of 1, 2, and 5
+    // DP[i] = sum(dp[i - arr[j]]) where i - arr[j] >= 0 and 0 <= j <= 2
+
+    int dp[1001];
+    dp[0] = 1;
+    for (int i{1}; i <= n; ++i) {
+        dp[i] = 0;
+    }
+
+    for (int i{0}; i <= n; ++i) {
+        for (int j{}; j < 3; ++j) {
+            if (i - arr[j] >= 0 && dp[i - arr[j]] != 0) {
+                dp[i] += (dp[i - arr[j]]) % 10007;
+            }
+        }
+    }
+
+    cout << dp[n];
+
+
+    return 0;
+}
